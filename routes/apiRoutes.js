49ab22c4d7dbbc52a1f71f39e8360efe51bda6bb
GageDorl <gagedorlando@gmail.com>
2019-11-23 12:23:36 -0500

    Gage (#10)
    * Made handlebars work and added create account part
    
    * Made user accounts with all required fields including public token for plaid
    
    * Get balance working
    
    * Transactions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
     // They won't get this or even be able to access this page if they aren't authed
     res.json("/members");
   });
-//
+  //
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
@@ -25,34 +25,36 @@ module.exports = function(app) {
       email: req.body.email,
       password: req.body.password,
       public_token: req.body.public_token
-    }).then(function() {
-      res.redirect(307, "/api/login");
-    }).catch(function(err) {
-      console.log(err);
-      res.json(err);
-      // res.status(422).json(err.errors[0].message);
-    });
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.json(err);
+        // res.status(422).json(err.errors[0].message);
+      });
   });
-//
+  //
   // Route for logging user out
   app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
-//
+  //
   // Route for getting some data about our user to be used client side
   app.get("/api/user_data", function(req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
-    }
-    else {
+    } else {
       // Otherwise send back the user's email and id
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         token: req.user.public_token,
         email: req.user.email,
-        id: req.user.id
+        id: req.user.id,
+        name: req.user.name
       });
     }
   });
